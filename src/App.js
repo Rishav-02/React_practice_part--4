03a564e13b9bcc258719a656481a49ec4ebaa0a8
Rishav <rishavsinha380@gmail.com>
2023-01-01 15:17:52 +0530

    Import and call all components in this File

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Hook from './useEffect/useEffect';
+import Specific from './useEffect/useEffect_with_specific_state_and_props';
+import Update from './useEffect/updateonprops';
+import Style from './Style/app';
+import Example from './Bootstrap/button';
+import VariantsExample from './Bootstrap/dropdown';
+import Array from './List/arraylist';
+import List from './List/list_with_bootstrap';
+import Nested from './List/nestedlist';
+import Reuse from './List/Reuse/ReuseComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Hook/>
+     <Specific/>
+     <Update/> <br/>
+     <Style/>
+     <Example/> <br/>
+     <VariantsExample/> <br/><br/>
+     <Array/> <br/>
+     <List/> <br/>
+     <Nested/> <br/>
+     <Reuse/>
     </div>
   );
 }
